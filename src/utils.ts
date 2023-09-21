79fbd601525d25607cfb78b9c2f109b4badcf53a
Ajaydeep Singh Rajpoot <49810031+Ajaydeep123@users.noreply.github.com>
2023-09-22 02:59:32 +0530

    Update utils.ts

--- src/utils.ts
+++ src/utils.ts
@@ -1,7 +1,7 @@
 import { Client } from 'pg';
 
 export async function getClient() {
-    const client = new Client("postgres://vrqgotiw:UTCF1iEialHmnqYIeFVo0j3wTsgFgDp1@peanut.db.elephantsql.com/vrqgotiw");
+    const client = new Client("URL of db");
     await client.connect();
     return client;
 }
